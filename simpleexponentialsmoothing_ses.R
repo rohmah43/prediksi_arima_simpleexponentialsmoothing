5c822fef10976ddbf34becc6f0452a9aa257259f
rohmah43 <73114027+rohmah43@users.noreply.github.com>
2021-02-12 09:22:39 +0700

    Add files via upload

---
+++ simpleexponentialsmoothing_ses.R
@@ -0,0 +1,18 @@
+install.packages("forecast", dependencies=TRUE)
+library(AnalyzeTS)
+library(MASS)
+library(tseries)
+library(forecast)
+library(lmtest) 
+
+dtinter <- read.csv("E:/Kuliah/PKL/databandara.csv",header = TRUE)
+dtinter
+dtintr <- ts(dtinter$Y, frequency = 12, start = c(1,1))
+dtintr
+plot(dtintr)
+
+#Fit simple exponential smoothing model to data and show summary
+fit_ses <- ses(dtintr, h = 16)
+summary(fit_ses)
+#Plot the forecasted values
+plot(fit_ses)
